14cf01034d9a990362edb0f924e1a91002cf2450
Holland <0195557@students.cheneysd.org>
2024-01-02 20:51:09 -0800

    updated navigation plan to add more

--- BlackhawkDesign.Web/src/App.vue
+++ BlackhawkDesign.Web/src/App.vue
@@ -1,29 +1,37 @@
 <template>
+
   <v-app id="vue-app">
     <v-app-bar color="primary">
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <router-link to="/" style="color: inherit">
+        <h2>Blackhawk Design</h2>
+      </router-link>
+      
       <v-toolbar-title>
         <router-link to="/" style="color: inherit">
-          Blackhawk Design
-          <img
-            src="./assets/logo.png"
-            width="64"
-            height="64"
-            alt="Blackhawk Design Logo"
-          />
+          <img src="./assets/logo.png"
+               width="64"
+               height="64"
+               alt="Blackhawk Design Logo" />
         </router-link>
+        
       </v-toolbar-title>
+
+      
+
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
       <v-btn @click=toggleTheme variant="flat" icon="mdi-home">t</v-btn>
+
     </v-app-bar>
-    <v-navigation-drawer v-model="drawer">
+    <v-navigation-drawer v-model="drawer" permanent
+        location="top">
       <v-list>
         <v-list-item link to="/">
+
+          <v-list-item-title>Home</v-list-item-title>
           <template #prepend>
             <v-icon>fas fa-home</v-icon>
           </template>
-          <v-list-item-title>Home</v-list-item-title>
         </v-list-item>
-
         <v-list-item link to="/jobs">
           <template #prepend>
             <v-icon>fas fa-bell</v-icon>
@@ -53,7 +61,9 @@
         </transition>
       </router-view>
     </v-main>
+
   </v-app>
+
 </template>
 
 <script setup lang="ts">
--- BlackhawkDesign.Web/src/components/HelloWorld.vue
+++ BlackhawkDesign.Web/src/components/HelloWorld.vue
@@ -1,21 +1,5 @@
 <template>
-  <v-parallax src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg">
-    <div
-      class="d-flex flex-column fill-height justify-center align-center text-white"
-    >
-      <h1 class="text-h4 font-weight-thin mb-4">
-        Blackhawk Design
-        <img
-          src="../assets/logo.png"
-          width="64"
-          height="64"
-          alt="Blackhawk Design Logo"
-        />
-      </h1>
-
-      <h4 class="subheading">cooper is hot</h4>
-    </div>
-  </v-parallax>
+  hi
 </template>
 
 <script setup lang="ts">
