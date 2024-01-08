3df0f81e734fbc67e2126f2300ce93d167d155aa
Holland H <0195557@cheneysd.org>
2024-01-08 14:07:52 -0800

    added mission statement ;)

--- BlackhawkDesign.Web/src/App.vue
+++ BlackhawkDesign.Web/src/App.vue
@@ -1,71 +1,67 @@
 <template>
-<div class="app">
-
-  <v-app id="vue-app" >
-    <v-app-bar color="primary">
-      <router-link to="/" style="color: inherit">
-        <h2 class="title">Blackhawk Design</h2>
-      </router-link>
-      
-      <v-toolbar-title>
+  <div class="app">
+    <v-app id="vue-app">
+      <v-app-bar color="primary">
         <router-link to="/" style="color: inherit">
-          <img src="./assets/logo.png"
-               width="64"
-               height="64"
-               alt="Blackhawk Design Logo" />
+          <h2 class="title">Blackhawk Design</h2>
         </router-link>
-        
-      </v-toolbar-title>
-      <v-btn href="/">Home</v-btn>
-      <v-btn href="/jobs">Jobs</v-btn>
-      <v-btn href="/apply">Apply</v-btn>
-      
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn @click=toggleTheme variant="flat" icon="mdi-home">t</v-btn>
 
-    </v-app-bar>
-    <v-navigation-drawer v-model="drawer" permanent
-        location="top">
-      <v-list>
-        <v-list-item link to="/">
+        <v-toolbar-title>
+          <router-link to="/" style="color: inherit">
+            <img
+              src="./assets/logo.png"
+              width="64"
+              height="64"
+              alt="Blackhawk Design Logo"
+            />
+          </router-link>
+        </v-toolbar-title>
+        <v-btn href="/">Home</v-btn>
+        <v-btn href="/jobs">Jobs</v-btn>
+        <v-btn href="/apply">Apply</v-btn>
 
-          <v-list-item-title>Home</v-list-item-title>
-          <template #prepend>
-            <v-icon>fas fa-home</v-icon>
-          </template>
-        </v-list-item>
-        <v-list-item link to="/jobs">
-          <template #prepend>
-            <v-icon>fas fa-bell</v-icon>
-          </template>
-          <v-list-item-title>Jobs</v-list-item-title>
-        </v-list-item>
-        <v-list-item link to="/apply">
-          <template #prepend>
-            <v-icon>fas fa-cubes</v-icon>
-          </template>
-          <v-list-item-title>Apply</v-list-item-title>
-        </v-list-item>
-        <v-list-item link to="/admin">
-          <template #prepend>
-            <v-icon>fas fa-cogs</v-icon>
-          </template>
-          <v-list-item-title>Admin Pages</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
+        <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+        <v-btn @click="toggleTheme" variant="flat" icon="mdi-home">t</v-btn>
+      </v-app-bar>
+      <v-navigation-drawer v-model="drawer" permanent location="top">
+        <v-list>
+          <v-list-item link to="/">
+            <v-list-item-title>Home</v-list-item-title>
+            <template #prepend>
+              <v-icon>fas fa-home</v-icon>
+            </template>
+          </v-list-item>
+          <v-list-item link to="/jobs">
+            <template #prepend>
+              <v-icon>fas fa-bell</v-icon>
+            </template>
+            <v-list-item-title>Jobs</v-list-item-title>
+          </v-list-item>
+          <v-list-item link to="/apply">
+            <template #prepend>
+              <v-icon>fas fa-cubes</v-icon>
+            </template>
+            <v-list-item-title>Apply</v-list-item-title>
+          </v-list-item>
+          <v-list-item link to="/admin">
+            <template #prepend>
+              <v-icon>fas fa-cogs</v-icon>
+            </template>
+            <v-list-item-title>Admin Pages</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
 
-    <v-main>
-      <!-- https://stackoverflow.com/questions/52847979/what-is-router-view-key-route-fullpath -->
-      <router-view v-slot="{ Component, route }">
-        <transition name="router-transition" mode="out-in" appear>
-          <component :is="Component" :key="route.path" />
-        </transition>
-      </router-view>
-    </v-main>
-
-  </v-app>
-</div>
+      <v-main>
+        <!-- https://stackoverflow.com/questions/52847979/what-is-router-view-key-route-fullpath -->
+        <router-view v-slot="{ Component, route }">
+          <transition name="router-transition" mode="out-in" appear>
+            <component :is="Component" :key="route.path" />
+          </transition>
+        </router-view>
+      </v-main>
+    </v-app>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -93,23 +89,17 @@ $medium: 900px;
   opacity: 0;
 }
 
-.main {
-    
-}
-
-
-
 .app {
   @media screen and (max-width: $small) {
     .app {
-      display:none
+      display: none;
     }
   }
 
   @media app and (min-width: $medium) {
     .app {
-      visibility:hidden
-  }
+      visibility: hidden;
+    }
   }
 }
 </style>
--- BlackhawkDesign.Web/src/views/Home.vue
+++ BlackhawkDesign.Web/src/views/Home.vue
@@ -1,16 +1,63 @@
 <template>
   <v-parallax
-    src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg"
+    src="../assets/homepage.png"
   >
     <div class="d-flex flex-column fill-height justify-center align-center text-white">
-      <h1 class="text-h4 font-weight-thin mb-4">
-        Vuetify
+      
+      <h1 class="test">
+        Blackhawk Design
+        <img src="../assets/logo.png"
+             width="64"
+             height="64"
+             alt="Blackhawk Design Logo" />
       </h1>
       <h4 class="subheading">
-        Build your application today!
+        Empowering the body of CHS!
       </h4>
     </div>
   </v-parallax>
+  <div class="mission">
+
+    <v-card
+    class="mx-auto my-12"
+    max-width="300"
+    
+  >
+
+    <v-img
+      cover
+      height=250
+      src="../assets/missionStatement.png"
+    ></v-img>
+
+    <v-card-item>
+      <v-card-title>Our Mission</v-card-title>
+    </v-card-item>
+
+    <v-card-text>
+      This website is mainly used for the student body of Cheney High School.
+      Our primary objective is to provide students the opportunity to look at available jobs and learn how they can benefit from our resources.
+      We evaluate who fits the qualifications for our jobs based on job screenings and skill checks as a precaution to better our business and quality of production.
+    </v-card-text>
+
+
+  </v-card>
+  </div>
+  <div class="skills">
+    
+  </div>
+  <div class="faq">
+    <h1 align="center">Frequently Asked Questions</h1>
+    <v-expansion-panels variant="popout" class="my-4">
+      <v-expansion-panel
+        v-for="i in 3"
+        :key="i"
+        title="Item"
+        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+      ></v-expansion-panel>
+    </v-expansion-panels>
+    
+  </div>
   <Footer></Footer>
 </template>
 
@@ -26,4 +73,14 @@ useTitle("Home");
   text-align: center;
 
 }
+
+.mission {
+  height: 550px;
+}
+
+.test {
+  font-family: 'Times New Roman', Times, serif
+}
+
+
 </style>
